be831103ddd92bb9bfb646888e9cddb1eb952e23
jamesrea1 <jamesreadev@outlook.com>
2021-02-10 08:27:42 +0000

    Mobile first homepage layout

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-/*
-Toggle mobile nav
-*/
+// /*
+// Toggle mobile nav
+// */
 
-const navToggleBtn = document.querySelector("#navToggleBtn");
-const siteNav = document.querySelector("#siteNav");
-const navToggleIconOpen = document.querySelector("#navToggleIconOpen");
-const navToggleIconClose = document.querySelector("#navToggleIconClose");
-const overlay = document.getElementById("overlay");
+// const navToggleBtn = document.querySelector("#navToggleBtn");
+// const siteNav = document.querySelector("#siteNav");
+// const navToggleIconOpen = document.querySelector("#navToggleIconOpen");
+// const navToggleIconClose = document.querySelector("#navToggleIconClose");
+// const overlay = document.getElementById("overlay");
 
-navToggleBtn.addEventListener("click", toggleNav)
-document.addEventListener('click', navToggleClickAway)
+// navToggleBtn.addEventListener("click", toggleNav)
+// document.addEventListener('click', navToggleClickAway)
 
-function toggleNav(){
-    const hidden = siteNav.classList.toggle("site-nav--is-visible");    
-    navToggleIconOpen.classList.toggle("nav-toggle__icon--is-visible");
-    navToggleIconClose.classList.toggle("nav-toggle__icon--is-visible");
-    toggleOverlay();
-}
+// function toggleNav(){
+//     const hidden = siteNav.classList.toggle("site-nav--is-visible");    
+//     navToggleIconOpen.classList.toggle("nav-toggle__icon--is-visible");
+//     navToggleIconClose.classList.toggle("nav-toggle__icon--is-visible");
+//     toggleOverlay();
+// }
 
-function toggleOverlay(){
-    overlay.classList.toggle("overlay--is-visible")
-}
+// function toggleOverlay(){
+//     overlay.classList.toggle("overlay--is-visible")
+// }
 
-function navToggleClickAway(e){
-    if(!navToggleBtn.contains(e.target) && siteNav.classList.contains("site-nav--is-visible")){
-        toggleNav();
-    }
-}
+// function navToggleClickAway(e){
+//     if(!navToggleBtn.contains(e.target) && siteNav.classList.contains("site-nav--is-visible")){
+//         toggleNav();
+//     }
+// }
